cf86d104ef42c1d40e8cd95f8bc90ca3e2c21f59
Mega.D <phamanhduc30012005@gmail.com>
2024-12-28 03:43:46 +0700

    Homepage Demo

--- Homepage.php
+++ Homepage.php
@@ -86,7 +86,7 @@
                       <i class="ti ti-list-check fs-6"></i>
                       <p class="mb-0 fs-3">My Task</p>
                     </a>
-                    <a href="./authentication-login.html" class="btn btn-outline-secondary mx-3 mt-2 d-block">Logout</a>
+                    <a href="./Sign_In.php" class="btn btn-outline-secondary mx-3 mt-2 d-block">Đăng xuất</a>
                   </div>
                 </div>
               </li>
